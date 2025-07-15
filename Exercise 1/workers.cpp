4dd5aed3f6b7a57074c6af2e2600127e196e8cb9
Logan.Schejbal <loganschejbal@gmail.com>
2025-07-15 01:29:52 -0500

    Class Structure Setup

---
+++ Exercise 1/workers.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include "workers.h"
+using namespace std;
+
+//Employee Class Functions
+employee::employee(){
+
+}
+employee::employee(int, string, double, string){
+
+}
+int employee::getID(){
+
+}
+void employee::setID(int){
+
+}
+string employee::getName(){
+
+}
+void employee::setName(string){
+
+}
+double employee::getAge(){
+
+}
+void employee::setAge(double){
+
+}
+string employee::getEducation(){
+
+}
+void employee::setEducation(string){
+
+}
+
+//Worker Class Functions
+string worker::getHiringDate(){
+
+}
+void worker::setHiringDate(int, int, int){
+
+}
+string worker::getDept(){
+
+}
+void worker::setDept(string){
+
+}
+float worker::getRate(){
+
+}
+void worker::setRate(float){
+
+}
+void worker::displayInfo(){
+
+}
+
+//Manager Class Functions
+string manager::getPromoDate(){
+
+}
+void manager::setPromoDate(string){
+
+}
+string manager::getDept(){
+
+}
+void manager::setDept(string){
+
+}
+string manager::getTitle(){
+
+}
+void manager::setTitle(string){
+
+}
+float manager::getSalary(){
+
+}
+void manager::setSalary(float){
+
+}
+void manager::displayInfo(){
+    
+}
---
+++ Exercise 1/workers.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class employee{
+    private:
+    int ID;
+    string name;
+    double age;
+    string education;
+
+    public:
+    employee();
+    employee(int, string, double, string);
+    int getID();
+    void setID(int);
+    string getName();
+    void setName(string);
+    double getAge();
+    void setAge(double);
+    string getEducation();
+    void setEducation(string);
+};
+
+class worker : employee {
+    private:
+    string hiringDate;
+    string dept;
+    float rate;
+
+    public:
+    string getHiringDate();
+    void setHiringDate(int, int, int);
+    string getDept();
+    void setDept(string);
+    float getRate();
+    void setRate(float);
+    void displayInfo();
+};
+
+class manager : employee {
+    private:
+    string promoDate;
+    string dept;
+    string title;
+    float salary;
+
+    public:
+    string getPromoDate();
+    void setPromoDate(string);
+    string getDept();
+    void setDept(string);
+    string getTitle();
+    void setTitle(string);
+    float getSalary();
+    void setSalary(float);
+    void displayInfo();
+};
