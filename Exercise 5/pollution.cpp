4f67191aa831cc2c9d1e99eb9ff2d76a227fc74b
seerupj <jada.seerup@gmail.com>
2025-07-20 23:09:07 -0500

    Update pollution.cpp

--- Exercise 5/pollution.cpp
+++ Exercise 5/pollution.cpp
@@ -3,42 +3,53 @@
 #include "pollution.h"
 using namespace std;
 
-bicycle::bicycle(int tempLength, char tempDiet){
+Bicycle::Bicycle(int tempLength, char tempDiet) {
     rideLength = tempLength;
     diet = tempDiet;
 }
-int bicycle::getCarbonFootprint() const {
+
+int Bicycle::getCarbonFootprint() const {
     int calories = rideLength * 65;
+    int footprint = 0;
     if (diet == 'p') {
-        return calories / 1000;
+        footprint = calories / 1000;
     } else if (diet == 'm') {
-        return calories * 5 / 1000;
+        footprint = calories * 5 / 1000;
     }
-    return 0;
+    cout << "[Bicycle] Ride Length: " << rideLength << " miles, Diet: " << (diet == 'p' ? "Plant-based" : "Meat-based")
+         << " ➔ Estimated Carbon Footprint: " << footprint << " kg CO2" << endl;
+    return footprint;
 }
 
-
-building::building(int tempElectric, int tempGas, int tempWater){
+Building::Building(int tempElectric, int tempGas, int tempWater) {
     electricUsage = tempElectric;
     gasUsage = tempGas;
     waterUsage = tempWater;
 }
-int building::getCarbonFootprint() const {
-    return static_cast<int>(electricUsage * 0.39 + gasUsage * 0.0544 + waterUsage * 0.002);
+
+int Building::getCarbonFootprint() const {
+    int footprint = static_cast<int>(electricUsage * 0.39 + gasUsage * 0.0544 + waterUsage * 0.002);
+    cout << "[Building] Electricity: " << electricUsage << " kWh, Gas: " << gasUsage
+         << " m³, Water: " << waterUsage << " m³ ➔ Estimated Carbon Footprint: "
+         << footprint << " kg CO2" << endl;
+    return footprint;
 }
 
-
-
-car::car(float tempEconomy, int tempMiles, char tempFuel){
+Car::Car(float tempEconomy, int tempMiles, char tempFuel) {
     fuelEconomy = tempEconomy;
     miles = tempMiles;
     fuel = tempFuel;
 }
-int car::getCarbonFootprint() const {
+
+int Car::getCarbonFootprint() const {
+    int footprint = 0;
     if (fuel == 'g') {
-        return static_cast<int>((miles / fuelEconomy) * 8.89);
+        footprint = static_cast<int>((miles / fuelEconomy) * 8.89);
     } else if (fuel == 'd') {
-        return static_cast<int>((miles / fuelEconomy) * 10.16);
+        footprint = static_cast<int>((miles / fuelEconomy) * 10.16);
     }
-    return 0; // Default case if fuel type is unknown
+    cout << "[Car] Miles: " << miles << ", Fuel Economy: " << fuelEconomy
+         << " MPG, Fuel Type: " << (fuel == 'g' ? "Gasoline" : "Diesel")
+         << " ➔ Estimated Carbon Footprint: " << footprint << " kg CO2" << endl;
+    return footprint;
 }
